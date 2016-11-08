79eb6276d541563658637697465e10106c3cad92
Corey <coreymberman@gmail.com>
2016-11-07 22:21:41 -0600

    residual blows up

--- project2.py
+++ project2.py
@@ -19,7 +19,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def gauss(A, b, x, epsilon = 1e-4):
+def gauss(A, b, x, alpha, epsilon = 1e-5):
 	'Run n iterations to solve Ax=b using Gauss-Sidel'
 	# thanks http://austingwalters.com/gauss-seidel-method/
 	L = np.tril(A)
@@ -29,6 +29,7 @@ def gauss(A, b, x, epsilon = 1e-4):
 	while res > epsilon:
 		x_star = x # initial guess
 		x = np.dot(np.linalg.inv(L), b - np.dot(U, x_star))
+		x = x*alpha + x_star*(1-alpha)
 		res = residual(x, x_star)
 
 	return x
@@ -38,11 +39,12 @@ def residual(a,b):
 
 def sourceTerm(eta, lamda):
 	'Linearize the source term S into S_C + S_P*theta'
-	S_C = np.full(len(eta), 0, float)
-	S_P = lamda*(1-np.square(eta))
+	#S_C = np.full(len(eta), 0, float)
+	S_C = np.square(lamda) * (1-np.square(eta))
+	S_P = np.full(len(eta), 0, float)
 	return S_C, S_P
 
-def coefficients(eta, d_eta, Bi, S_C, S_P):
+def coefficients(eta, d_eta, Bi, lamda, S_C, S_P):
 	'Returns the coefficients to solve a_P*T_P = a_E*T_E + a_W*T_W + b'
 	N = len(eta)
 	A = np.zeros(N**2).reshape(N, N)
@@ -54,40 +56,40 @@ def coefficients(eta, d_eta, Bi, S_C, S_P):
 		# A[i][i++] is like aE
 		if i == 0:
 			# symmetric BC
-			A[i][i+1] = 2*(eta[i]+d_eta/2)/d_eta # 2* because aW=aE and TW=TE
-			A[i][i] = -1*(A[i][i+1] - S_P[i]*eta[i]*d_eta/2)
-			b[i] = S_C[i]*eta[i]*d_eta/2
+			A[i][i+1] = 1*(1/d_eta**2)/lamda
+			A[i][i] = (S_P[i] - 2/d_eta**2)/lamda
+			b[i] = -1*S_C[i]
 		elif i == N-1:
 			# wall heat flux BC
-			A[i][i-1] = 1*(eta[i]-d_eta/2)/d_eta
-			A[i][i] = -1*(A[i][i-1] - S_P[i]*eta[i]*d_eta/2 + Bi)
-			b[i] = S_C[i]*eta[i]*d_eta/2 + 0 # 0 for h_e*T_inf ... when its nondimensionalized, T_inf => theta = 0. not sure if this is correct
+			A[i][i-1] = (1/d_eta**2 - 1/(eta[i]*d_eta))/lamda
+			A[i][i] = (S_P[i] - 2/d_eta**2)/lamda
+			b[i] = -1*S_C[i] + Bi
 		else:
-			A[i][i-1] = 1*(eta[i]-d_eta/2)/d_eta
-			A[i][i+1] = 1*(eta[i]+d_eta/2)/d_eta
-			A[i][i] = -1*(A[i][i-1] + A[i][i+1] - S_P[i]*eta[i]*d_eta/2)
-			b[i] = S_C[i]*eta[i]*d_eta/2
+			A[i][i-1] = (1/d_eta**2 - 1/(eta[i]*d_eta))/lamda
+			A[i][i+1] = (1/d_eta**2 + 1/(eta[i]*d_eta))/lamda
+			A[i][i] = (S_P[i] - 2/d_eta**2)/lamda
+			b[i] = -1*S_C[i]
 	return A, b
 
 def solve_lamda(eta, phi):
 	y = phi*eta*(1-np.square(eta))
 	lamda = 1./4.*1./np.trapz(y, eta)
-	print "solving lamda.."
-	print "y="+str(y)
-	print "lamda="+str(lamda)
-	return -1*lamda
+	#print "y="+str(y)
+	return 1*lamda
 
 if __name__ == '__main__':
 	#setup grid
-	N = 50
+	N = 15
 	eta = np.linspace(0, 1, N, True)
 	d_eta = eta[1]-eta[0]
 	print "NEW RUN"
 	print "eta="+str(eta)
 	print "d_eta="+str(d_eta)
 
-	Bi = 1
+	Bi = 5
 	phi = np.ones(N)
+
+	debug = 0
 
 	epsilon = 1e-5
 	res = epsilon + 1
@@ -95,15 +97,20 @@ if __name__ == '__main__':
 		phi_star = phi
 		lamda = solve_lamda(eta, phi_star)
 		S_C, S_P = sourceTerm(eta, lamda)
-		print "S_C="+str(S_C)
-		print "S_P="+str(S_P)
-		A, b = coefficients(eta, d_eta, Bi, S_C, S_P)
-		print "A="+str(A)
-		print "b="+str(b)
-		phi = gauss(A, b, phi_star, 1e-5)
+		
+		A, b = coefficients(eta, d_eta, Bi, lamda, S_C, S_P)
+		
+		phi = gauss(A, b, phi_star, .3, epsilon)
 
 		res = residual(phi, phi_star)
-		print "theta="+str(lamda*phi)
+		
+		if debug:
+			print "S_C="+str(S_C)
+			print "S_P="+str(S_P)
+			print "A="+str(A)
+			print "b="+str(b)
+			print "lamda="+str(lamda)
+			print "theta="+str(lamda*phi)
 		print "res="+str(res)
 
 	theta = lamda*phi
