36a55a0759533bdd6fa394319ab7f63bf1f5fdcd
Corey <coreymberman@gmail.com>
2016-11-08 18:18:56 -0600

    near end of class

--- project2.py
+++ project2.py
@@ -37,10 +37,16 @@ def gauss(A, b, x, alpha, epsilon = 1e-5):
 def residual(a,b):
 	return np.sum(np.absolute(a - b))
 
-def sourceTerm(eta, lamda):
+def solve_lamda(eta, phi):
+	y = phi*eta*(1-np.square(eta))
+	lamda = 1./4.*1./np.trapz(y, eta)
+	#print "y="+str(y)
+	return 1*lamda
+
+def sourceTerm(eta, lamda, phi):
 	'Linearize the source term S into S_C + S_P*theta'
 	#S_C = np.full(len(eta), 0, float)
-	S_C = np.square(lamda) * (1-np.square(eta))
+	S_C = np.square(lamda) * (1-np.square(eta)) * phi
 	S_P = np.full(len(eta), 0, float)
 	return S_C, S_P
 
@@ -56,60 +62,58 @@ def coefficients(eta, d_eta, Bi, lamda, S_C, S_P):
 		# A[i][i++] is like aE
 		if i == 0:
 			# symmetric BC
-			A[i][i+1] = 1*(1/d_eta**2)/lamda
-			A[i][i] = (S_P[i] - 2/d_eta**2)/lamda
-			b[i] = -1*S_C[i]
+			A[i][i+1] = -0.5
+			A[i][i] = 1 #-S_P[i]*(d_eta/2)**2
+			b[i] = S_C[i]*(d_eta/2)**2
 		elif i == N-1:
 			# wall heat flux BC
-			A[i][i-1] = (1/d_eta**2 - 1/(eta[i]*d_eta))/lamda
-			A[i][i] = (S_P[i] - 2/d_eta**2)/lamda
-			b[i] = -1*S_C[i] + Bi
+			A[i][i-1] = -2/d_eta
+			A[i][i] = 1/d_eta - (2-Bi*d_eta**2)
+			b[i] = 0
 		else:
-			A[i][i-1] = (1/d_eta**2 - 1/(eta[i]*d_eta))/lamda
-			A[i][i+1] = (1/d_eta**2 + 1/(eta[i]*d_eta))/lamda
-			A[i][i] = (S_P[i] - 2/d_eta**2)/lamda
-			b[i] = -1*S_C[i]
+			A[i][i-1] = -1*(eta[i]/d_eta - 0.5)
+			A[i][i+1] = -1*(eta[i]/d_eta + 0.5)
+			A[i][i] = -A[i][i+1] + -A[i][i-i] - S_P[i]*eta[i]*d_eta
+			b[i] = S_C[i]*eta[i]*d_eta
 	return A, b
 
-def solve_lamda(eta, phi):
-	y = phi*eta*(1-np.square(eta))
-	lamda = 1./4.*1./np.trapz(y, eta)
-	#print "y="+str(y)
-	return 1*lamda
+def enforceBC(phi):
+	return 1
 
 if __name__ == '__main__':
 	#setup grid
-	N = 15
+	N = 6
 	eta = np.linspace(0, 1, N, True)
 	d_eta = eta[1]-eta[0]
 	print "NEW RUN"
 	print "eta="+str(eta)
 	print "d_eta="+str(d_eta)
 
-	Bi = 5
+	Bi = 1
 	phi = np.ones(N)
 
-	debug = 0
+	debug = 1
 
 	epsilon = 1e-5
 	res = epsilon + 1
 	while res > epsilon:
 		phi_star = phi
 		lamda = solve_lamda(eta, phi_star)
-		S_C, S_P = sourceTerm(eta, lamda)
+		S_C, S_P = sourceTerm(eta, lamda, phi_star)
 		
 		A, b = coefficients(eta, d_eta, Bi, lamda, S_C, S_P)
-		
-		phi = gauss(A, b, phi_star, .3, epsilon)
-
-		res = residual(phi, phi_star)
-		
 		if debug:
 			print "S_C="+str(S_C)
 			print "S_P="+str(S_P)
 			print "A="+str(A)
 			print "b="+str(b)
 			print "lamda="+str(lamda)
+
+		phi = gauss(A, b, phi_star, .3, epsilon)
+
+		res = residual(phi, phi_star)
+		
+		if debug:
 			print "theta="+str(lamda*phi)
 		print "res="+str(res)
 
