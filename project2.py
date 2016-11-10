7a1553bcdd905dc48585ac4b62e2da008b7f337b
Corey <coreymberman@gmail.com>
2016-11-09 20:39:58 -0600

    converges, result looks incorrect

--- project2.py
+++ project2.py
@@ -10,7 +10,7 @@
 #      1/(4*integral(phi*eta*(1-eta^2)d_eta, 0,1))
 #  3) With lamda_star, solve the ODE
 #      lamda*(1/eta)*(d/d_eta)*(eta*d_phi/d_eta) + 
-#        lamda^2*(1-eta^2)*phi = 0
+#        lamda*(1-eta^2)*phi = 0
 #  4) Iterate until |phi-phi_star| <= epsilon
 #  5) theta = lamda * phi
 #     theta(r=R) = theta_wall
@@ -46,13 +46,13 @@ def solve_lamda(eta, phi):
 def sourceTerm(eta, lamda, phi):
 	'Linearize the source term S into S_C + S_P*theta'
 	#S_C = np.full(len(eta), 0, float)
-	S_C = np.square(lamda) * (1-np.square(eta)) * phi
+	S_C = lamda * (1-np.square(eta)) * phi
 	S_P = np.full(len(eta), 0, float)
 	return S_C, S_P
 
-def coefficients(eta, d_eta, Bi, lamda, S_C, S_P):
+def coefficients(eta, d_eta, S_C, S_P):
 	'Returns the coefficients to solve a_P*T_P = a_E*T_E + a_W*T_W + b'
-	N = len(eta)
+	N = len(eta) # exclude first and last point BCs
 	A = np.zeros(N**2).reshape(N, N)
 	b = np.zeros(N)
 
@@ -61,28 +61,31 @@ def coefficients(eta, d_eta, Bi, lamda, S_C, S_P):
 		# A[i][i-1] is like aW
 		# A[i][i++] is like aE
 		if i == 0:
-			# symmetric BC
-			A[i][i+1] = -0.5
-			A[i][i] = 1 #-S_P[i]*(d_eta/2)**2
-			b[i] = S_C[i]*(d_eta/2)**2
-		elif i == N-1:
-			# wall heat flux BC
-			A[i][i-1] = -2/d_eta
-			A[i][i] = 1/d_eta - (2-Bi*d_eta**2)
+			#A[i][i] = (d_eta/2)**2*S_P[i]
+			#A[i][i+1] = 0
+			#b[i] = S_C[i]*(d_eta/2)**2
+			A[i][i] = 1e-6 #needs to be small but nonzero to avoid singular matrix error
+		elif i >= N-1:
+			A[i][i] = 1/d_eta - (2-d_eta**2*Bi)
+			A[i][i-1] = 2/d_eta
 			b[i] = 0
 		else:
+			A[i][i] = 1*(eta[i]/d_eta + 0.5) + 1*(eta[i]/d_eta - 0.5) - S_P[i]*eta[i]*d_eta
 			A[i][i-1] = -1*(eta[i]/d_eta - 0.5)
 			A[i][i+1] = -1*(eta[i]/d_eta + 0.5)
-			A[i][i] = -A[i][i+1] + -A[i][i-i] - S_P[i]*eta[i]*d_eta
 			b[i] = S_C[i]*eta[i]*d_eta
 	return A, b
 
-def enforceBC(phi):
-	return 1
+def symmBC(nb):
+	return nb
+
+def fluxBC(nb, d_eta, Bi):
+	return nb/(1 + d_eta*Bi)
+
 
 if __name__ == '__main__':
 	#setup grid
-	N = 6
+	N = 50
 	eta = np.linspace(0, 1, N, True)
 	d_eta = eta[1]-eta[0]
 	print "NEW RUN"
@@ -92,24 +95,28 @@ if __name__ == '__main__':
 	Bi = 1
 	phi = np.ones(N)
 
-	debug = 1
+	debug = 0
 
-	epsilon = 1e-5
+	epsilon = 1e-4
 	res = epsilon + 1
 	while res > epsilon:
 		phi_star = phi
 		lamda = solve_lamda(eta, phi_star)
 		S_C, S_P = sourceTerm(eta, lamda, phi_star)
 		
-		A, b = coefficients(eta, d_eta, Bi, lamda, S_C, S_P)
+		A, b = coefficients(eta, d_eta, S_C, S_P)
 		if debug:
 			print "S_C="+str(S_C)
 			print "S_P="+str(S_P)
 			print "A="+str(A)
+			print "phi*="+str(phi_star)
 			print "b="+str(b)
 			print "lamda="+str(lamda)
 
-		phi = gauss(A, b, phi_star, .3, epsilon)
+		
+		phi = gauss(A, b, phi_star, .6, 1e-5)
+		phi[0] = symmBC(phi[1])
+		phi[-1] = fluxBC(phi[-2], d_eta, Bi)
 
 		res = residual(phi, phi_star)
 		
